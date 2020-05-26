9e99b541b43c2571c0b13470b42a39e49449a088
Matt Stevans <stevans@utexas.edu>
2020-05-26 01:02:00 -0500

    initial commit”

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data and code to analyze the demographic that moved away from Bernie between 2016 and 2020 in Texas..',
+    author='Matt Stevans',
+    license='MIT',
+)
